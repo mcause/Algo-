e066621c13d607722d6e616c2605665300e15c55
mcause <m.cause88@gmail.com>
2021-12-19 07:48:32 -0500

    converting a number to a string

---
+++ convert-number-to-string.js
@@ -0,0 +1,10 @@
+// Convert a Number to a String!
+
+// 123 --> "123"
+// 999 --> "999"
+
+
+function numberToString(num) {
+    // Return a string of the number here!]
+    return (num.toString())
+  }
