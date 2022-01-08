7aa34f804d556bb6ae93f112813324a9442846b8
mcause <m.cause88@gmail.com>
2022-01-08 09:32:33 -0500

    completed all lvl 8 codewars

--- even-odd.js
+++ even-odd.js
@@ -5,3 +5,10 @@ function even_or_odd(number) {
     return number % 2 === 0 ? "Even" : "Odd"
   }
   
+  function even_or_odd(number) {
+    if (number%2 == 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+  }
