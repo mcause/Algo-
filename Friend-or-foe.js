8c11f4966535bb0a0302ee007909b14fbac0add3
mcause <m.cause88@gmail.com>
2022-03-03 23:02:31 -0500

    completed a few more codewars lvl 7

--- Friend-or-foe.js
+++ Friend-or-foe.js
@@ -17,3 +17,10 @@ function friend(friends){
       return item.length === 4 
     });  
   }
+
+         // or
+
+function friend(friends){
+//your code here
+return friends.filter( friend => (friend.length === 4))
+        }
